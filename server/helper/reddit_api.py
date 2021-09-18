10af81dd65d57d0eb0ac4e0d00a3459372376eaf
DhruvBhatia0 <69252327+DhruvBhatia0@users.noreply.github.com>
2021-09-18 03:03:30 -0400

    redditbackend

---
+++ server/helper/reddit_api.py
@@ -0,0 +1,24 @@
+#! python3
+
+"""personal use script = wHCu_ThCqGDSWzyRDzF0fA
+security code = 2_wnaaHJYOvf4atHB_6i12X08hKBCA"""
+
+import praw
+
+reddit = praw.Reddit(client_id='wHCu_ThCqGDSWzyRDzF0fA', \
+                     client_secret='2_wnaaHJYOvf4atHB_6i12X08hKBCA', \
+                     user_agent='Stock Bot', \
+                     username='the_real_mayo', \
+                     password='Sunf10wer')
+
+subreddit = reddit.subreddit('wallstreetbets').search("google")
+#sussy = subreddit.top(limit = 10)
+temp = 0
+for i in subreddit:
+    if len(i.selftext) == 0:
+        continue
+    temp +=1
+
+    f = open(f"./corpus/{temp}_reddit.txt", 'w', encoding='utf-8')
+    f.write(i.selftext)
+    f.close()
