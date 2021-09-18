5cc1804d6f785b2a4b13d472e455efee3c94bd2a
mehtaViraj <mehta.viraj.03@gmail.com>
2021-09-18 13:28:44 -0400

    making json (in progress, jomills help me)

--- server/helper/yfinance_api.py
+++ server/helper/yfinance_api.py
@@ -1,11 +1,23 @@
 import yfinance as yf
 import json
-which = input("Which stock: ")
-stock = yf.Ticker(which)
-details = stock.info
-hist = stock.history(period="max")
-hist = hist[-30:]
-del hist['Volume']
-final = hist.to_json()
-print(final)
-quarterly = stock.quarterly_earnings
+
+def getStockData(which):
+    stock = yf.Ticker(which)
+    hist = stock.history(period="max")
+    hist = hist[-30:]
+    del hist['Volume']
+    history = hist.to_dict()
+    print(type(history))
+    print(history)
+    quarterly = stock.quarterly_earnings.to_json()
+    #print(type(quarterly))
+
+    final = {}
+    final['name'] = which
+    final['quarterly'] = quarterly
+    final['history'] = history
+
+    result = json.dumps(final, indent=4)
+    #print(result)
+
+getStockData('MSFT')
