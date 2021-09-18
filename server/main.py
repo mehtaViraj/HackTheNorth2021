787d70a0ba2b36abbc55c4f9e6c992ca4a73ff7a
mehtaViraj <mehta.viraj.03@gmail.com>
2021-09-17 22:47:13 -0400

    templatified old server, initialized api based flask server

--- server/main.py
+++ server/main.py
@@ -1,14 +1,4 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import flask
+from flask import request, jsonify
 
-class helloHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        #200 is the status response code
-        self.send_header('content-type','text/html')
-        self.end_headers()
-        self.wfile.write('hello'.encode())
-
-PORT = 8080
-server = HTTPServer(('',PORT), helloHandler)
-print(f'Server running on port {PORT}')
-server.serve_forever()
+#New main file
