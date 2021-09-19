63bd40cb927bad2c426f090a3c0c11ac876d414e
DhruvBhatia0 <69252327+DhruvBhatia0@users.noreply.github.com>
2021-09-18 20:22:51 -0400

    added function to good bad

--- server/helper/good_or_bad.py
+++ server/helper/good_or_bad.py
@@ -1,4 +1,7 @@
 import pickle
+import os
+
+
 f = open('good_words.dat', 'rb')
 good_words = pickle.load(f)
 f.close()
@@ -7,6 +10,13 @@ f = open('bad_words.dat', 'rb')
 bad_words = pickle.load(f)
 f.close()
 
+def get_text(company):
+    txt = ''
+    for file in os.listdir(f"corpus_{company}"):
+        with open(os.path.join(f"corpus_{company}", file), encoding="utf-8") as ofile:
+            txt += ofile.read()
+
+    return txt
 def check(text):
     text = text.upper()
     text = list(text.split(' '))
